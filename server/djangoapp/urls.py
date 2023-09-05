b7b8119d96219eed410e935cfc867968c57c867e
mhosler <marcushoslerwork@gmail.com>
2023-09-05 10:47:35 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,24 +5,32 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
     # path for about view
+    path('about/', views.about, name='about'),
 
     # path for contact us view
+    path('contact/', views.contact, name='contact'),
 
     # path for registration
+    # Uncomment below when you've defined registration_request view
+    # path('register/', views.registration_request, name='register'),
 
     # path for login
+    # Uncomment below when you've defined login_request view
+    # path('login/', views.login_request, name='login'),
 
     # path for logout
+    # Uncomment below when you've defined logout_request view
+    # path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    # Uncomment below when you've defined get_dealer_details view
+    # path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_reviews'),
 
     # path for add a review view
+    # Uncomment below when you've defined add_review view
+    # path('dealer/<int:dealer_id>/add_review/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,17 +13,15 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # Create your views here.
 
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
-
+def about(request):
+    return render(request, 'djangoapp/about.html')
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -42,7 +40,6 @@ def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
